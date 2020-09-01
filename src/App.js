07bee3034cb1345f64411d6256c1a1a307ab6f38
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-31 23:50:46 -0400

    Added routing, privacy policy component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
+import Privacy from "./components/privacy";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/privacy-policy">
+          <Privacy />
+        </Route>
+        <Route path="/terms-of-service">
+          <Temp />
+        </Route>
+        <Route path="/">
+          <Temp />
+        </Route>
+      </Switch>
+    </Router>
   );
+}
+
+const Temp = () => {
+  return (
+    <div>
+      <h1>TEMP</h1>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/privacy.js
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+import privacy from "../data/privacy-docs";
+
+const Wrapper = styled.div`
+  width: 50em;
+  margin: auto;
+`
+
+const Title = styled.div`
+  font-size: 5em;
+  text-align: center;
+  color: #40C3A0;
+
+  &.date {
+    color: grey;
+    font-size: 1.5em;
+  }
+
+  &.subtitle {
+    margin-top: 1em;
+    font-size: 2.5em;
+    color: #67CFB3;
+  }
+`
+
+const Info = styled.div`
+  margin: 5px;
+  font-size: 1.23em;
+  font-weight: 200;
+  text-align: center;
+`
+
+
+const Privacy = () => {
+  return (
+    <Wrapper>
+      <Title>
+        Privacy Policy
+      </Title>
+      <Title className="date">
+        Effective Date: {privacy.date}
+      </Title>
+      <Info>
+        {privacy.policy}
+      </Info>
+      <Title className="subtitle">
+        What Information Do We Collect
+      </Title>
+      <Info>
+        {privacy.information}
+      </Info>
+      <Title className="subtitle">
+        Why and How Do We Use Your Information
+      </Title>
+      <Info>
+        {privacy.geolocation}
+      </Info>
+      <Title className="subtitle">
+        Changes Made to the Privacy Policy
+      </Title>
+      <Info>
+        {privacy.changes}
+      </Info>
+      <Title className="subtitle">
+        Contact Us
+      </Title>
+      <Info>
+        {privacy.contact}
+      </Info>
+    </Wrapper>
+  )
+}
+
+export default Privacy;
